200060bf9a590fa43caca866b4877b256b5270fa
JoaquinCamino12 <joaquincamino09@gmail.com>
2026-07-20 19:43:34 -0300

    Correccion deploy

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
 'http://localhost:5173', 
-'http://127.0.0.1:5173']
+'http://127.0.0.1:5173']
